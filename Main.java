23af3616ac3c40ddb68586c8b04576fcea21f76b
Jorge Santtana <jhonnsantt4na@gmail.com>
2024-09-18 22:19:10 -0300

    JAVA
    Iniciando Primeiros passos com o Javinha

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Olá Mundo com o Java
+        System.out.println("Hello World!");
+
+        // Operadores Aritimeticos = + - / * %
+        // Ordem de Precedencia =
+        // 1°:(),
+        // 2°: / * %,
+        // 3°: + -
+
+        // Tipos de dados no JAVA
+        // Inteiros:
+        byte num = 21;
+        short num2 = 22;
+        int num3 = 23;
+        long num4 = 24;
+        // Ponto Flutuantes
+        float width = 1;
+        double height = 1.68;
+        // Unicode e Caracteres
+        char sex = 'F';
+        String nome = "Jorge";
+        // Valor verdade
+        boolean isAdmin = true;
+
+        // Saidas de Dados
+        System.out.println(nome);
+        System.out.print(num3);
+        // Com o println = Pula uma Linha no Final
+    }
+}
