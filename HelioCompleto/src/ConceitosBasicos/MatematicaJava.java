37d5429ef0729fdfb82b46d60d417443bbcdb430
Santt4na <jhonnsantt4na@gmail.com>
2024-09-22 10:02:36 -0300

    Add BasicConception in Java

--- HelioCompleto/src/ConceitosBasicos/MatematicaJava.java
+++ HelioCompleto/src/ConceitosBasicos/MatematicaJava.java
@@ -1,4 +1,38 @@
 package ConceitosBasicos;
 
+import java.sql.SQLOutput;
+
 public class MatematicaJava {
+  public static void main(String[] args) {
+    // math in java :
+    // Math.sqrt(value)           = Square Root (Raiz_Quadrada)
+    // Math.pow(value, elevated)  = Value elevated (Potência)
+    // Math.abs(value)            = Value Absolute (Absoluto)
+
+    double value1 = 3.0;
+    double value2 = 4.0;
+    double value3 = -5.0;
+    double res1, res2, res3;
+
+    res1 = Math.sqrt(value1);
+    res2 = Math.sqrt(value2);
+    res3 = Math.sqrt(25.0);
+    System.out.printf("Square Root at %.1f is > %.1f %n", value1, res1);
+    System.out.printf("Square Root at %.1f is > %.1f %n", value2, res2);
+    System.out.printf("Square Root at 25.0 is > %.1f %n", res3);
+
+    res1 = Math.pow(value1, value2);
+    res2 = Math.pow(value2, 2.0);
+    res3 = Math.pow(5.0, 2.0);
+    System.out.println(res1);
+    System.out.println(res2);
+    System.out.println(res3);
+
+    res1 = Math.abs(value1);
+    res2 = Math.abs(value2);
+    res3 = Math.abs(value3);
+    System.out.println(res1);
+    System.out.println(res2);
+    System.out.println(res3);
+  }
 }
--- HelioCompleto/src/ConceitosBasicos/NextLineScanner.java
+++ HelioCompleto/src/ConceitosBasicos/NextLineScanner.java
@@ -1,4 +1,24 @@
 package ConceitosBasicos;
 
+import java.util.Scanner;
+
 public class NextLineScanner {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    String s1, s2, s3;
+    int x;
+
+    x = sc.nextInt();
+    sc.nextLine();      // when we use another type, we need to do it like this
+    s1 = sc.nextLine(); // get all the text until you press enter
+    s2 = sc.nextLine();
+    s3 = sc.nextLine();
+
+    System.out.println(x);
+
+    System.out.println(s1);
+    System.out.println(s2);
+    System.out.println(s3);
+  }
 }
