2f16cd830ce07f3805b5069fe4f797b92bf32404
Santt4na <jhonnsantt4na@gmail.com>
2024-09-23 21:30:06 -0300

    Exercicios IF

---
+++ HelioCompleto/src/Exercicios/EstruturaCondicao/DuracaoPartida.java
@@ -0,0 +1,25 @@
+package Exercicios.EstruturaCondicao;
+import java.util.Scanner;
+
+public class DuracaoPartida {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+
+      int horaInicial = sc.nextInt();
+      int horaFinal = sc.nextInt();
+      int duracao;
+
+      if (horaInicial < horaFinal) {
+        duracao = horaFinal - horaInicial;
+      } else if (horaInicial > horaFinal) {
+        duracao = 24 - horaInicial + horaFinal;
+      } else {
+        duracao = 24;
+      }
+
+      System.out.println("O JOGO DUROU " + duracao + " HORA(S)");
+
+      sc.close();
+    }
+}
+
--- HelioCompleto/src/Exercicios/EstruturaCondicao/ParImpar.java
+++ HelioCompleto/src/Exercicios/EstruturaCondicao/ParImpar.java
@@ -2,17 +2,17 @@ package Exercicios.EstruturaCondicao;
 
 import java.util.Scanner;
 
-public class Negativo {
+public class ParImpar {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("Write the number:");
+    System.out.println("Write one value:");
     int value = sc.nextInt();
 
-    if(value < 0){
-      System.out.println("NEGATIVO");
+    if(value % 2 == 0){
+      System.out.println("PAR");
     }else{
-      System.out.println("NÂO NEGATIVO");
+      System.out.println("IMPAR");
     }
     sc.close();
   }
