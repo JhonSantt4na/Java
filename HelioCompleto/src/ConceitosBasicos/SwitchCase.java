59099d198086579d4d36274e31ef02d19b111180
Santt4na <jhonnsantt4na@gmail.com>
2024-09-24 22:49:02 -0300

    While end Exames for while ]

---
+++ HelioCompleto/src/ConceitosBasicos/SwitchCase.java
@@ -0,0 +1,41 @@
+package ConceitosBasicos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class SwitchCase {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    String dia;
+
+    switch (x) {
+      case 1:
+        dia = "Domingo";
+        break;
+      case 2:
+        dia = "Segunda";
+        break;
+      case 3:
+        dia = "Terça";
+        break;
+      case 4:
+        dia = "Quarta";
+        break;
+      case 5:
+        dia = "Quinta";
+        break;
+      case 6:
+        dia = "Sexta";
+        break;
+      case 7:
+        dia = "Sabado";
+        break;
+      default:
+        dia = "Value Invalid";
+        break;
+    }
+    System.out.println("Dia da Semana: " + dia);
+    sc.close();
+  }
+}
