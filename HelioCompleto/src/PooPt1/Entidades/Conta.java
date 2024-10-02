dd69888d5b426e79ea377d7dd11d216f8aca2720
Santt4na <jhonnsantt4na@gmail.com>
2024-10-01 23:50:02 -0300

    ExeFixação

---
+++ HelioCompleto/src/PooPt1/Entidades/Conta.java
@@ -0,0 +1,55 @@
+package PooPt1.Entidades;
+
+public class Conta {
+  public int number;
+  private String holder;
+  private double balance;
+
+  private double depositoInicial = 0;
+
+  public String toString() {
+    return  "Account " +
+            number+
+            ", Holder: "+
+            holder+
+            ", Balance: $ "+
+            String.format("%.2f", balance);
+  }
+
+
+  public Conta(int numeroConta,String nomeTitular) {
+    this.number = numeroConta;
+    this.holder = nomeTitular;
+  }
+
+  public Conta(int numeroConta, String nomeTitular, double depositoInicial) {
+    this.number = numeroConta;
+    this.holder = nomeTitular;
+    deposito(depositoInicial);
+  }
+
+  public int getNumeroConta() {
+    return number;
+  }
+
+  public String getNomeTitular(){
+    return holder;
+  }
+
+  public void setNomeTitular(String nomeTitular){
+    this.holder = nomeTitular;
+  }
+
+  public double getSaldo(){
+    return balance;
+  }
+
+  public void deposito(double saldo){
+    this.balance += saldo;
+  }
+
+  public void saque(double saque){
+      this.balance -= saque + 5.00;
+  }
+
+}
---
+++ HelioCompleto/src/PooPt1/ExercioPoo/ContaBancaria.java
@@ -0,0 +1,58 @@
+package PooPt1.ExercioPoo;
+
+import PooPt1.Entidades.Conta;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaBancaria {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+    Conta newAcc;
+
+    int number;
+    String holder;
+    char resposta;
+    double depositInitial;
+
+    System.out.println("Enter account number: ");
+    number = sc.nextInt();
+
+    System.out.println("Enter account holder: ");
+    holder = sc.nextLine();
+    
+    sc.nextLine();
+
+    System.out.println("Is there na initial deposit (y/n) ? ");
+    resposta = sc.next().charAt(0);
+
+    if (resposta == 'y'){
+      System.out.println("Enter initial deposit value: ");
+      depositInitial = sc.nextDouble();
+      newAcc = new Conta(number, holder, depositInitial );
+    }else {
+      newAcc = new Conta(number,holder);
+    }
+
+    System.out.println();
+    System.out.println("Account data:");
+    System.out.println(newAcc);
+
+    System.out.println();
+    System.out.print("Enter a deposit value: ");
+    double depositValue = sc.nextDouble();
+    newAcc.deposito(depositValue);
+    System.out.println("Updated account data:");
+    System.out.println(newAcc);
+
+    System.out.println();
+    System.out.print("Enter a withdraw value: ");
+    double saqueValue = sc.nextDouble();
+    newAcc.saque(saqueValue);
+    System.out.println("Updated account data:");
+    System.out.println(newAcc);
+
+    sc.close();
+  }
+}
