5e65e34375a484e68840deae5b84c8707d0bb8f1
Santt4na <jhonnsantt4na@gmail.com>
2024-10-03 23:00:06 -0300

    updated: Arrays/Vetores

--- HelioCompleto/src/Memoria_Vetores/Vetores.java
+++ HelioCompleto/src/Memoria_Vetores/Vetores.java
@@ -5,30 +5,27 @@ import java.util.Scanner;
 
 public class Vetores {
   public static void main(String[] args) {
-    //todos do mesmo tipo
-    //ordenado por posições
-    //Alocada de uma vez so em um bloco contiguo de memoria
-    // vantag: Acesso imediato pela sua posição
-    // desvtg: Tamanho fixo, Dificudade para se realizar inserções e deleções
-
-    //Ex:
     Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
 
     int n = sc.nextInt();
-    //inicia com 0 se for numero pq o java ver como double
+
+    // Criando o vetor com possições indefinida
+    // Ou seja o valor que eu definir sera a qtd de elemento do vetor
     double[] vect = new double[n];
 
+    // Guardando dados nos elementos
+    // for para acessar as possição do vetor
     for (int i = 0; i < n; i++) {
       vect[i] = sc.nextDouble();
     }
-    
-    double soma = 0.0;
-    for (int i = 0; i < n; i++) {
-      soma+=vect[i];
+    double soma = 0.0; // Declaramos um acumulador soma
+    for (int i = 0; i < n; i++) {  // Rodamos pelo array
+      soma += vect[i];              // adicionamos o valor do arry ao acumulador cada passagem soma
     }
-    double avg = soma / n;
-    System.out.printf("Average Height: %.2f ", avg);
+    double avg = soma / n;     // com os valores somado, dividimos para ter a media
+
+    System.out.println(avg);
 
     sc.close();
   }
