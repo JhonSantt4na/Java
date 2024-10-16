8b90b0ad36474dcaafc9e58094f4a15f28ad09b7
Santt4na <jhonnsantt4na@gmail.com>
2024-10-15 23:34:02 -0300

    Testando Jva Class

---
+++ HelioCompleto/src/HerancaPolimorfismo/Testando.java
@@ -0,0 +1,13 @@
+package HerancaPolimorfismo;
+
+public class Testando {
+	public String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(nome);
+	}
+}
