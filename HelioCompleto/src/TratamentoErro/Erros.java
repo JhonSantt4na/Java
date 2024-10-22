00d313703f7f7fa1ff1fbaa0c4942370bdedeb5c
Santt4na <jhonnsantt4na@gmail.com>
2024-10-21 23:04:05 -0300

    Tratativa de Erros

---
+++ HelioCompleto/src/TratamentoErro/Erros.java
@@ -0,0 +1,16 @@
+package TratamentoErro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Erros {
+	public static void main(String[] args) {
+		List<String> lista = new ArrayList<>();
+		try {
+		
+		}catch (){
+		
+		}
+		
+	}
+}
