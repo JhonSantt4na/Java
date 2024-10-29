49e0c312f7af346823d31589bb128651667d2375
Santt4naDev <jhonnsantt4na@gmail.com>
2024-10-29 20:22:58 -0300

    Manipulando Arquivos

---
+++ HelioCompleto/src/Arquivos/FileWriterBufferedWriter.java
@@ -0,0 +1,33 @@
+package Arquivos;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterBufferedWriter {
+    /* FileWrite: (Stream de escrita de caracteres em arquivos)
+    para instanciarmos podemos usar:
+    Cria/Recriar > new FileWriter(Caminho)
+    Acrescentar a arquivo existente : new FileWrite(Caminho, true)
+
+    BufferedWrite
+    * */
+
+    public static void main(String[] args) {
+        String[] lines = new String[] {"Good morning", "Good Afternoon", "Good Night"};
+        String path = "/home/jhondev/IdeaProjects/Java/HelioCompleto/src/Arquivos/out.txt";
+
+        // Para Ultilizar o mesmo arquivo existente
+        // try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
+            for(String line: lines){
+                bw.write(line);
+                bw.newLine();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ HelioCompleto/src/Arquivos/TryWithResources.java
@@ -0,0 +1,28 @@
+package Arquivos;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResources {
+    // é um bloco try qye declara um ou mais recursos, e garante
+    // que esses recursos serão fechados ao final do bloco > disponivel JAVA 7+
+    public static void main(String[] args) {
+
+        String path = "/home/jhondev/IdeaProjects/Java/HelioCompleto/src/Arquivos/input.txt";
+        // Não precisamos mas instanciar da forma antiga e sim dessa forma
+        // Diretamente no try instanciamos os dois juntos
+        try(BufferedReader br = new BufferedReader( new FileReader(path))){
+            // Lendo uma linha porem se tiver no final retorna um null
+
+            String line = br.readLine();
+            while(line != null){
+                System.out.println(line);
+                line = br.readLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+}
