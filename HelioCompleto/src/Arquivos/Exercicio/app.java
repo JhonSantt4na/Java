96523e20b70ece37e20c07a0feadb49fa2ea46ca
Santt4naDev <jhonnsantt4na@gmail.com>
2024-10-30 19:49:49 -0300

    Tentando Resolver

---
+++ HelioCompleto/src/Arquivos/Exercicio/app.java
@@ -0,0 +1,15 @@
+package Arquivos.Exercicio;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class app {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter with path file: ");
+        String path = sc.nextLine();
+
+
+        sc.close();
+    }
+}
