5ea52a3d1f7903c5bf43e25b0d655afb4273da17
Santt4naDev <jhonnsantt4na@gmail.com>
2024-11-03 11:08:07 -0300

    Intro a Interfaces

---
+++ HelioCompleto/src/Interfaces/Solution01/Services/RentalService.java
@@ -0,0 +1,42 @@
+package Interfaces.Solution01.Services;
+
+import Interfaces.Solution01.Entities.CarRental;
+import Interfaces.Solution01.Entities.Invoice;
+
+import java.time.Duration;
+
+public class RentalService {
+	private Double pricePerDay;
+	private Double pricePerHours;
+	
+	private BrazilTaxService taxService;
+	
+	public RentalService(Double pricePerHours, Double pricePerDay, BrazilTaxService taxService) {
+		this.pricePerHours = pricePerHours;
+		this.pricePerDay = pricePerDay;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice (CarRental carRental){
+		// Pegando a diff de duas datas e convertendo em minutos
+		double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+		// Assim pegamos o valor das horas por fração
+		
+		double hours = minutes / 60.0;
+		
+		double basicPayment;
+		if (hours <= 12.0) {
+			// Usar tarifa por hora e arredondar para cima
+			basicPayment = pricePerHours * Math.ceil(hours);
+		} else {
+			// Usar tarifa por dia
+			basicPayment = pricePerDay * Math.ceil(hours / 24.0);
+		}
+		
+		// Usando o service da Taxa
+		double tax = taxService.tax(basicPayment);
+		
+		// Usando o Serviço
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+}
