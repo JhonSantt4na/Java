6867620407208aa005a61389f6deefb3d6e7f548
Santt4naDev <jhonnsantt4na@gmail.com>
2024-11-04 20:51:59 -0300

    vanois

--- HelioCompleto/src/Interfaces/asdsa/asd.java
+++
@@ -1,7 +0,0 @@
-package Interfaces.asdsa;
-
-public class asd {
-	public static void main(String[] args) {
-		System.out.println("Teste");
-	}
-}
---
+++ HelioCompleto/src/Interfaces/duvi/kjj.java
@@ -0,0 +1,7 @@
+package Interfaces.duvi;
+
+public class kjj {
+	public static void main(String[] args) {
+		System.out.println("Amor");
+	}
+}
