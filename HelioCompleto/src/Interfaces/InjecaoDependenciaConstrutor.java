034cd7f213de86b16cc7ad39fd66f1b561862506
Santt4naDev <jhonnsantt4na@gmail.com>
2024-11-05 23:12:08 -0300

    Interface

---
+++ HelioCompleto/src/Interfaces/InjecaoDependenciaConstrutor.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+public class InjecaoDependenciaConstrutor {
+	// Basicamente é o fato da na nossa RentalService no construtor passa a nossa
+	// Interface que tera o tipo TaxService que sera nossa Interface,
+	// sendo que o BrazilTaxService ele implementa a nossa Inrerface TaxService
+	// no nosso app principal podemos chamar o metodo BrasilTaxService que neste caso
+	// de Instanciação ocorrera o Upcasting automaticamente.
+	
+	// Forte Acoplamento = quando for mudar algo sempre tera 2 pontos de alteração
+}
