bba51b1a673d77ce1edf383942ac548d286f58c1
Santt4naDev <jhonnsantt4na@gmail.com>
2024-11-08 09:57:29 -0300

    HerdarVsCumpriContrato

---
+++ HelioCompleto/src/Interfaces/HerdarVsCumpriContrato.java
@@ -0,0 +1,23 @@
+package Interfaces;
+
+public class HerdarVsCumpriContrato {
+	// Aspectos em comum entre herança e interfaces
+	// Relação é um :
+	// Generalização/especialização : Tipos genericos e especificos
+	//Polimorfismo : o gererico pode ser associada em tempo de ex com os childs
+	
+	// Diferenca:
+	// Herança > Reuso
+	// Interface > Contrato a ser cumprido
+	
+	// reusamos a superclasse na sub classe
+	// ja na interface é tipo assim
+	// que foi da classe da interface tem que ter o tal methodo
+	
+	// Porem podemos ter uma combinação das duas ex;
+	// interface > SuperClasse Abstrata > SubClasse
+	//  area()	  >	- color	                   	 >  calc para sua area()
+	
+	// Sendo assim nem toda figura precisa ter cor.
+	
+}
