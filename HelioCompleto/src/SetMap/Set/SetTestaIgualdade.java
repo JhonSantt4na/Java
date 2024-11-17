4cfbbac6a10c37bb193bb9c77280edfa507a4e19
Santt4naDev <jhonnsantt4na@gmail.com>
2024-11-17 13:49:52 -0300

    como o set testa igualdade e o TreeSet compara Elementos

---
+++ HelioCompleto/src/SetMap/Set/SetTestaIgualdade.java
@@ -0,0 +1,59 @@
+package SetMap.Set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTestaIgualdade {
+	//  Como as coleções Hash testam igualdade?
+	
+		// Se hashCode e equals estiverem implementados:
+			// Primeiro hashCode. Se der igual, usa equals para confirmar.
+			// Lembre-se: String, Integer, Double, etc. já possuem equals e hashCode
+	
+		// Se hashCode e equals NÃO estiverem implementados:
+			// Compara as referências (ponteiros) dos objetos. (referencias do obj)
+	
+	// classe que não contem hashCode e equals implementado
+	public static class Product {
+		private String name;
+		private Double price;
+		public Product(String name, Double price) {
+			this.name = name;
+			this.price = price;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public Double getPrice() {
+			return price;
+		}
+		public void setPrice(Double price) {
+			this.price = price;
+		}
+	}
+	
+	// Program princp
+	public static void main(String[] args) {
+		Set<Product> set = new HashSet<>();
+		
+		set.add(new Product("TV", 900.0));
+		set.add(new Product("Notebook", 1200.0));
+		set.add(new Product("Tablet", 400.0));
+		
+		Product prod = new Product("Notebook", 1200.0);
+		
+		// testando se esse conjunto tem no nosso set Product
+		System.out.println(set.contains(prod)); // false
+		
+		// Como não tem a implementação hashCode e iquals na Class Product
+		// ele compara  a instanciação ou seja cada "new Product("ex") " é um obj
+		// diferente
+		
+		// Para comparar o conteudo vamos implementar o hashCode Iquals
+		// na nossa class Product e ai sim ele return true pois ele compara por conteudo
+		// e não pela referencia de obj
+	}
+}
---
+++ HelioCompleto/src/SetMap/Set/TreeSetComparaElementos.java
@@ -0,0 +1,10 @@
+package SetMap.Set;
+
+public class TreeSetComparaElementos {
+	// Recordando as implementações
+	// Principais implementações:
+		// HashSet - mais rápido (operações O(1) em tabela hash) e não ordenado
+		// TreeSet - mais lento (operações O(log(n)) em árvore rubro-negra) e ordenado pelo compareTo do objeto (ou Comparator)
+		// LinkedHashSet - velocidade intermediária e elementos na ordem em que são adicionados
+
+}
