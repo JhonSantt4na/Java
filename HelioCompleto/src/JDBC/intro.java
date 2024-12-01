cdfe23d09e9ad1e1bffc61012c749648e25e21cd
Santt4naDev <jhonnsantt4na@gmail.com>
2024-11-30 23:04:00 -0300

    JDBC (Java Database Connectivity)

---
+++ HelioCompleto/src/JDBC/intro.java
@@ -0,0 +1,8 @@
+package JDBC;
+
+public class intro {
+	// JDBC (Java Database Connectivity): API padrão do Java para acesso a dados
+	// Pacotes: java.sql e javax.sql (API suplementar para servidores)
+	
+	//
+}
