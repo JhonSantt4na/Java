eefa6ae60ec0fe612e827c5ae02e94434b50d664
Santt4naDev <jhonnsantt4na@gmail.com>
2024-11-30 22:01:05 -0300

    Programação Funcional (Pipelane)

---
+++ HelioCompleto/src/ProgramacaoFuncional/Streams/Pipeline.java
@@ -0,0 +1,26 @@
+package ProgramacaoFuncional.Streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Pipeline {
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+		
+		Stream<Integer> st1 = list.stream().map(x->x *10);
+		System.out.println(Arrays.toString(st1.toArray()));
+		
+		// Somando todos os items da lista
+		int sum = list.stream().reduce(0, (x, y) -> x + y);
+		System.out.println("Sum = " + sum);
+		
+		List<Integer> newlist = list.stream() // criando uma stream pela a list inicial
+			.filter(x -> x%2 == 0) // Filtrou os numeros pares
+			.map(x -> x * 10) // Multiplicou eles por 10
+			.collect(Collectors.toList()); // Transformou uma coleção em lista novamente
+		
+		System.out.println(Arrays.toString(newlist.toArray()));
+	}
+}
--- HelioCompleto/src/ProgramacaoFuncional/Streams/implementacaoStreams.java
+++ HelioCompleto/src/ProgramacaoFuncional/Streams/implementacaoStreams.java
@@ -1,6 +1,5 @@
 package ProgramacaoFuncional.Streams;
 
-import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -14,71 +13,72 @@ public class implementacaoStreams {
 	//Stream é uma solução para processar sequências de dados de forma:
 	
 	//• Declarativa (iteração interna: escondida do programador)
-	//• Parallel-friendly (imutável -> thread safe) facilidade de usar paralelismo e concorencia com streams
+	//• Parallel-friendly (imutável -> thread safe) facilidade de usar paralelismo e concorencia com o uso de streams
 	//• Sem efeitos colaterais
-	//• Sob demanda (lazy evaluation) evolução tardia.
+	//• Sob demanda (lazy evaluation) evolução tardia. Os dados so seram consumidos qunado for necessario
 	
 	//• Acesso sequencial (não há índices)
-	//• Single-use: só pode ser "usada" uma vez ( gera execção )
+	//• Single-use: só pode ser "usada" uma vez ( usar mais de 1x gera execção )
 	
-	//• Pipeline = Linha de montagem: operações em streams retornam novas streams.
+	//• Pipeline
+	// Linha de montagem: operações em streams retornam novas streams.
 	// Então é possível criar uma cadeia de operações (fluxo de processamento).
 	
-	// Operações intermediárias e terminais
+	// Operações Agregadas intermediárias e terminais
 	
 	//• O pipeline é composto por zero ou mais operações intermediárias e uma terminal.
 	
 	//• Operação intermediária:
 		//• Produz uma nova streams (encadeamento)
 		//• Só executa quando uma operação terminal é invocada (lazy evaluation)
-	// Operações intermediárias
-	//• filter
-	//• map
-	//• flatmap
-	//• peek
-	//• distinct
-	//• sorted
-	//• skip
-	//• limit (*) =
 	
-	// * short-circuit pois corta a operação quando a condição é satisfeita
+	// Operações intermediárias
+	//• filter, map, flatmap, peek, distinct, sorted, skip, limit (*) =
+	
+	// * short-circuit  = corta a operação quando a condição é satisfeita
 	
 	
 	//• Operação terminal:
 		//• Produz um objeto não-stream (coleção ou outro)
 		//• Determina o fim do processamento da stream
+	
 	// Operações terminais
-	//• forEach
-	//• forEachOrdered
-	//• toArray
-	//• reduce
-	//• collect
-	//• min
-	//• max
-	//• count
-	//• anyMatch (*)
-	//• allMatch (*)
-	//• noneMatch (*)
-	//• findFirst (*)
-	//• findAny (*)
-	//* short-circuit
+	//• forEach, forEachOrdered, toArray, reduce, collect, min, max ,count,
+	// anyMatch (*), allMatch (*), noneMatch (*), findFirst (*), findAny (*)
+	
+	//com * são operações short-circuit
 	
 	// Criar uma stream
+	
 	//• Basta chamar o metodo stream() ou parallelStream() a partir
 	//de qualquer objeto Collection.
+	
 	//• Outras formas de se criar uma stream incluem:
-	//• Stream.of
-	//• Stream.ofNullable
-	//• Stream.iterate
+	//• Stream.of, Stream.ofNullable, Stream.iterate
+	
 	public static void main(String[] args) {
-		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
-		Stream<Integer> st1 = list.stream();
+		// Criamos um array com elementos de Inteiros
+		List<Integer> list = Arrays.asList(3,4,5,10,7);
+		// Criamos uma Stream apartir de uma coleção
+		Stream<Integer> st0 = list.stream();
+		
+		// Usando o map para interar com os elementos
+		Stream<Integer> st1 = list.stream().map(x -> x*10);
+		
+		// Exibindo na tela usamos o toArray
 		System.out.println(Arrays.toString(st1.toArray()));
-		Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
+		
+		// Criando uma Stream de String
+		Stream<String> st2 = Stream.of("Maria", "Alex", "Bob"); // Streams.of Usado para criar um Stream com items dentro
 		System.out.println(Arrays.toString(st2.toArray()));
-		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
-		System.out.println(Arrays.toString(st3.limit(10).toArray()));
-		Stream<Long> st4 = Stream.iterate(new long[]{ 0L, 1L }, p->new long[]{ p[1], p[0]+p[1] }).map(p -> p[0]);
+		
+		// Criando uma Stream usando a Função de iteração
+		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2 ); // Cria uma Stream com o elemento 0 e uma interação adicionando +2 pra sempre
+		System.out.println(Arrays.toString(st3.limit(10).toArray())); // Usamos o limit para limitar em 10 elementos e depois transformamos para array para ser exibido
+		
+		// Gerando Stream Sequencia de Fibbonaci
+		Stream<Long> st4 = Stream.iterate(new Long[]{0L, 1L} , p -> new Long[] { p[1] ,  p[0] + p[1] } ).map(p -> p[0]);
+		// Map para pegar cada par para a pos 0
 		System.out.println(Arrays.toString(st4.limit(10).toArray()));
 	}
 }
