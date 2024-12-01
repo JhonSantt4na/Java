030626c45bddf6d6d6639392eb2194d9047bda9b
Santt4naDev <jhonnsantt4na@gmail.com>
2024-12-01 13:49:53 -0300

    JDBC (MySQL + OperacoesBasicas )

--- HelioCompleto/src/DataBase_JDBC/JDBC.java
+++ HelioCompleto/src/DataBase_JDBC/JDBC.java
@@ -1,8 +1,8 @@
-package JDBC;
+package DataBase_JDBC;
 
-public class intro {
+public class JDBC {
 	// JDBC (Java Database Connectivity): API padrão do Java para acesso a dados
 	// Pacotes: java.sql e javax.sql (API suplementar para servidores)
+	// Java Applicacation > JDBC > JDBC Drive (DB *) > DB *
 	
-	//
 }
