98921b516d168e5d286d9f0f641353d1c1056584
Santt4naDev <jhonnsantt4na@gmail.com>
2024-12-11 23:33:02 -0300

    Add

---
+++ HelioCompleto/src/_12_DataBase_JDBC/Exemplo/Coments.java
@@ -0,0 +1,13 @@
+package _12_DataBase_JDBC.Exemplo;
+
+public class Coments {
+	private double price;
+	private String name;
+	private String email;
+	
+	public Coments(double price, String name, String email) {
+		this.price = price;
+		this.name = name;
+		this.email = email;
+	}
+}
