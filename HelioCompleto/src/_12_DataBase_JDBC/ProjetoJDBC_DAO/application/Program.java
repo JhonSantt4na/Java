cb12ce276e643e0744bdd512e552b7423a0634f0
Santt4naDev <jhonnsantt4na@gmail.com>
2024-12-14 21:46:42 -0300

    Insert implamatation

--- HelioCompleto/src/_12_DataBase_JDBC/ProjetoJDBC_DAO/application/Program.java
+++ HelioCompleto/src/_12_DataBase_JDBC/ProjetoJDBC_DAO/application/Program.java
@@ -28,6 +28,11 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n=== TEST 3: seller Insert =====");
+		Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inseted ! New id = " + newSeller.getId());
+		
 	}
 }
 
--- HelioCompleto/src/_12_DataBase_JDBC/ProjetoJDBC_DAO/model/DaoImpl/SellerDaoJDBC.java
+++ HelioCompleto/src/_12_DataBase_JDBC/ProjetoJDBC_DAO/model/DaoImpl/SellerDaoJDBC.java
@@ -6,10 +6,7 @@ import _12_DataBase_JDBC.ProjetoJDBC_DAO.model.InterfacesDao.SellerDao;
 import _12_DataBase_JDBC.ProjetoJDBC_DAO.model.entities.Department;
 import _12_DataBase_JDBC.ProjetoJDBC_DAO.model.entities.Seller;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -24,7 +21,39 @@ public class SellerDaoJDBC implements SellerDao {
 	
 	@Override
 	public void insert(Seller obj) {
-	
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+				"INSERT INTO seller "
+					+" (Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES (?, ?, ?, ?, ?)",
+				Statement.RETURN_GENERATED_KEYS);
+				
+				st.setString(1,  obj.getName());
+				st.setString(2, obj.getEmail());
+				st.setDate(3, new Date(obj.getBirthDate().getTime()));
+				st.setDouble(4, obj.getBaseSalary());
+				st.setInt(5, obj.getDepartment().getId());
+				
+				int rowsAffected = st.executeUpdate();
+				
+				if (rowsAffected > 0){
+					ResultSet rs = st.getGeneratedKeys();
+					if(rs.next()){
+						int id = rs.getInt(1);
+						obj.setId(id);
+					}
+					
+					DB.closeResultSet(rs);
+				}
+				else {
+					throw  new DBException("Unexpected Error ! No Rows Affected");
+				}
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 	
 	@Override
