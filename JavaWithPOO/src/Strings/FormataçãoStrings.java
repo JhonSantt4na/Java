b682707b2ef6cd5143557e4d778ba51b121b1f44
Santt4na <jhonnsantt4na@gmail.com>
2025-09-04 23:02:46 -0300

    FormataçãoDeStrings

--- JavaWithPOO/src/Strings/FormataçãoStrings.java
+++ JavaWithPOO/src/Strings/FormataçãoStrings.java
@@ -48,5 +48,77 @@ public class FormataçãoStrings {
 		     %h			  - 	qualquer tipo				          - string hexadecimal resultante do metodo hashcode()
 		     %t			  - 	data/hora(long, Calendar, Date e TemporalAccessor) - datas e horas formatadas com flags adicionais
 		* */
+		
+		// Exibição de um inteiro com diferentes especificadores:
+		int valorInteiro = 65;
+		System.out.printf("%d%n", valorInteiro);  // 65
+		System.out.printf("%o%n", valorInteiro);  // 101
+		System.out.printf("%#x%n", valorInteiro);  // 41   x em minuscule, X maiúsculas
+		System.out.printf("%#X%n", valorInteiro); // 0X41  prefixo # -> Exibe que é Hexadecimal
+		System.out.printf("%b%n", valorInteiro);  // true
+		System.out.printf("%c%n", valorInteiro);  // A
+		System.out.printf("%s%n", valorInteiro);  // 65
+		System.out.printf("%h%n", valorInteiro);  // 41 gerado pelo hashcode
+		
+		// Exibição de um double com diferentes especificadores:
+		double valorDouble = 62.75;
+		System.out.printf("%f%n",valorDouble);   // 62,750000 Exibe como decimal normal com 6 casas decimais
+		System.out.printf("%a%n",valorDouble);   // 0x1.f6p5  Exibe o valor decimal em hexadecimal
+		System.out.printf("%e%n",valorDouble);   // 6,275000e+01 Exibe o decimal em notação em científica
+		System.out.printf("%g%n",valorDouble);   // 62,7500 Exibe em notificação cientifica ou em notação comun com 4 digits após à vírgula o menor é escolhido
+		System.out.printf("%#a%n",valorDouble);  // 0x1.f6p5 Exibe em hexadecimal minusculas
+		System.out.printf("%#A%n",valorDouble);  // 0X1.F6P5 Exibe em hexadecimal Maiúsculas
+		System.out.printf("%b%n",valorDouble);   // true Exibe em booleano null ou nao
+		System.out.printf("%s%n",valorDouble);   // 62.75 Exibe em String
+		System.out.printf("%h%n",valorDouble);   // 404f6000 Hexadecimal com hashcode
+		
+		
+		// Símbolos complementos
+		double preco = 47.5;
+		System.out.printf("|%.2f|%n",preco);    // |47,50| .2 -> informa que será com 2 casas após a vírgula
+		System.out.printf("|%6.2f|%n",preco);   // | 47,50| 6. -> numero antes do ponto informa a quantidade de casas que vai complementa com espaços a esquerda
+		System.out.printf("|%-6.2f|%n",preco);  // |47,50 | -6. -> complementa com espaços a direita
+		System.out.printf("|%06.2f|%n",preco);  // |047,50| 06. -> complementa com 0
+		int qtd = 71;
+		System.out.printf("|%d|%n",qtd);     // |71|
+		System.out.printf("|%6d|%n",qtd);    // |    71|
+		System.out.printf("|%-6d|%n",qtd);   // |71    |
+		System.out.printf("|%06d|%n",qtd);   // |000071|
+	
+		// Separadores, Exibição de negativos e combinações
+		double saldoPos = 7654.321;
+		System.out.printf("|%.2f|%n",saldoPos);         // |7654,32| .2f -> Mostrando somente com 2 casas por mais que tenha 3
+		System.out.printf("|%,.2f|%n",saldoPos);        // |7.654,32| ,. -> Mostrando com os separadores de Milhar
+		
+		double saldoNeg = -7654.321;
+		System.out.printf("|% ,.2f|%n",saldoNeg);       // |-7.654,32|  ,.2f -> se positivo ele adicona um espaço, se nao exibe o valor negativo
+		System.out.printf("|% ,.2f|%n",saldoPos);       // | 7.654,32|
+		System.out.printf("|%+,.2f|%n",saldoNeg);       // |-7.654,32|
+		System.out.printf("|%+,.2f|%n",saldoPos);       // |+7.654,32| +,.2f-> ao inves de adicionar o espaço vazio adicionamos o sinal de '+'
+		System.out.printf("|%(,11.2f|%n",saldoNeg);		// | (7.654,32)| 11 espaços com separador de milhar negativo entre parenteses e positivo deixa espaço no inicio
+		System.out.printf("|%(,011.2f|%n",saldoPos);    // | 007.654,32|
+		
+		listarProdutos();
+		
 	}
+	
+	public static void listarProdutos() {
+		String[] nomes = new String[]
+			{"Abacaxi", "Banana", "Laranja", "Morango", "Uva"};
+		
+		double[] precos = new double[]
+			{4.25, 2.33, 2.99, 7.89, 11.79};
+		
+		int[] quantidades = new int[]
+			{423, 26, 365, 811, 712};
+		
+		System.out.printf("%-10s|%7s | %7s%n", "Nome", "Preço", "Estoque");
+		System.out.println("----------------------------");
+		for (int i = 0; i < nomes.length; i++) {
+			System.out.printf("%-10s|%7.2f | %07d%n", nomes[i], precos[i], quantidades[i]);
+		}
+		
+	}
+	
+	
 }
