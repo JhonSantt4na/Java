13117998a79533f04d5afb48ec79ae025f64e7fc
JhonSantt4na <jhonnsantt4na@gmail.com>
2026-02-05 18:18:34 -0300

    Project Finance - Implements User Controllers

--- Finance/src/main/java/com/santt4na/Finance/Controller/AccountController.java
+++ Finance/src/main/java/com/santt4na/Finance/Controller/AccountController.java
@@ -1,8 +1,12 @@
 package com.santt4na.Finance.Controller;
 
+import com.santt4na.Finance.entity.Account;
 import com.santt4na.Finance.services.AccountService;
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +18,9 @@ public class AccountController {
 	
 	private final AccountService accountService;
 	
-	
+	@GetMapping("/createAccount/{userId}")
+	public ResponseEntity<Account> CreateAccount(@PathVariable Long userId, Account account) throws Exception {
+		Account newAccount = accountService.create(userId, account);
+		return ResponseEntity.ok(newAccount);
+	}
 }
